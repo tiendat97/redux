f49e6092efec2a31a8b9e3d99dc06aff06f7ee87
tiendat97 <datdt16@fsoft.com.vn>
2018-07-17 13:52:02 +0700

    add features

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-
-ReactDOM.render( <App/>, document.getElementById("root"));
+var {Provider} = require('react-redux');
+ReactDOM.render( 
+    <App/>, 
+    document.getElementById("root")
+);
