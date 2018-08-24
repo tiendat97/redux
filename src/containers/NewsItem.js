4eed2fd01f92cf91ad228dc772ea5045c2547694
tiendat97 <datdt16@fsoft.com.vn>
2018-08-24 17:28:42 +0700

    apply rudux-saga

--- src/containers/NewsItem.js
+++ src/containers/NewsItem.js
@@ -16,11 +16,7 @@ let NewsItem = ({ article }) => (
             <div>
         List of task to do:
                 <h1>{article[0].id}. {article[0].task_todo}</h1>
-                <h1>{article[1].id}. {article[1].task_todo}</h1>
-                <h1>{article[2].id}. {article[2].task_todo}</h1>
-                <h1>{article[3].id}. {article[3].task_todo}</h1>
-                <h1>{article[4].id}. {article[4].task_todo}</h1>
-                <h1>{article[5].id}. {article[5].task_todo}</h1>
+        
             </div>
         </article> :
         null
